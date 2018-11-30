2283539a215e020705656e336554d4f5f9dcb705
chenyy <1280129660@qq.com>
2018-11-30 22:00:18 +0800

    数据结构之数组.

--- Array.java
+++ Array.java
@@ -8,29 +8,70 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Array {
+	private int[] data;
+	private int size;
 	
-	public enum Season{
-		Spring, Summer, Autumn, Winter
+	public Array(int capacity) {
+		data = new int[capacity];
+		size = 0;
 	}
 	
-	public static void main(String [] args) {
-		
-		ArrayList<String> c = new ArrayList<>();
-		
-		c.add("q");
-		c.add("a");
-		c.add("b");
-		c.add("c");
-		c.remove("q"); 
-		Object[] o = c.toArray();
-		System.out.println(o);
-		for (Object i : o) {
-			System.out.println(i);
+    //	无参数的构造函数
+	public Array() {
+		this(10);
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public int getCapacity() {
+		return data.length;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+    //	添加元素,向末尾添加一个元素
+	public void addLast(int e) {
+//		if(size == data.length) {
+//			throw new IllegalArgumentException("addLast failed.Array is full");
+//		}
+//		data[size] = e;
+//		size++;
+		add(size, e);
+	}
+	
+	public void addFirst(int e) {
+		add(0, e);
+	}
+	
+    //	向指定位置添加元素
+	public void add(int index, int e) {
+		if(size == data.length) {
+			throw new IllegalArgumentException("addLast failed.Array is full");
 		}
+		if (index > size || index < 0) {
+			throw new IllegalArgumentException("addLast failed.Index error");
+		}
+		
+		//指定位置的元素向后移动
+		for(int i=size-1; i>= index; i--) {
+			data[i+1] = data[i];
+			
+		}
+	    data[index] = e;
+		size++;
+		
+	}
+	
+	
+	
+	
 		
 
 	    
-	}
 	
 }
 
