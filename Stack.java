1bd7f5477f65e3e81c0c9b8848f7b7445cd2e465
chenyy <1280129660@qq.com>
2018-12-02 22:06:06 +0800

    添加stack接口类

---
+++ Stack.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package javaTest;
+
+/**
+ * @author chenyanyan
+ *
+ */
+public interface Stack<E> {
+	int getSize();
+	boolean isEmpty();
+	void push(E e);
+	E pop();
+	E peek();
+
+}
