b1b82ba1a1dbeb4ea5eaacb1fc0c1d13919715ec
chenyy <1280129660@qq.com>
2018-12-10 22:10:03 +0800

    添加元素方法

--- LinkedList.java
+++ LinkedList.java
@@ -25,6 +25,64 @@ public class LinkedList<E> {
 		}
 	}
 	
+	private Node head;
+	private int size;
+	
+	LinkedList(){
+		head = null;
+		size = 0;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public void addFirst(E e) {
+//		Node node = new Node(e);
+//		node.next = head;
+//		head = node;
+		head = new Node(e, head);
+		size++;
+		
+	}
+	
+	
+	public void add(int index, E e) {
+		
+		if (index < 0 || index > size) {
+			throw new IllegalArgumentException("Add failed. Illegal index.");
+		}
+		
+		if (index == 0) {
+			addFirst(e);
+		}
+		
+		else {
+			Node prv = head;
+			for(int i = 1; i < index - 1; i++) {
+				prv = prv.next;
+				
+			}
+			
+//			Node node = new Node(e);
+//			node.next = prv.next;
+//			prv.next = node;
+			prv.next = new Node(e, prv.next);
+			
+			
+			size++;
+		}
+		
+	}
+	
+	public void addLast(E e) {
+		add(size, e);
+	}
+	
 	
 
 	
